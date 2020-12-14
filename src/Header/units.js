604dd675f31dc816b364c13bfd292324e7422fb4
aryazano <alexander.ryazanov@megafon.ru>
2020-12-14 13:24:58 +0300

    style(header): changed height and font-size

--- src/Header/units.js
+++ src/Header/units.js
@@ -1,11 +1,12 @@
 import styled from "@emotion/styled";
 
 export const HeaderWrapper = styled.div`
-    height: 50px;
+    height: 70px;
     width: 100%;
     background-color: black;
     color: white;
     display: flex;
     justify-content: center;
     align-items: center;
+    font-size: 32px;
 `;
