b6c91b92083ecc6c6c485d6d14eedbe033ee7e8b
aryazano <alexander.ryazanov@megafon.ru>
2021-01-20 22:34:49 +0300

    style(contentWrapper): add scroll only to content

--- src/components/MainWrapper/units.js
+++ src/components/MainWrapper/units.js
@@ -2,10 +2,13 @@ import styled from "@emotion/styled";
 
 export const ContentWrapperWithMenu = styled.div`
     width: 100%;
+    height: calc(100vh - 70px);
     display: flex;
+    overflow-y: hidden;
 `;
 
 export const ContentWrapper = styled.div`
     width: 100%;
     padding: 25px;
+    overflow-y: auto;
 `;
