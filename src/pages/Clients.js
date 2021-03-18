835e309cef137fcbfb9e9d6a9252b56045caa4e5
aryazanov <alexander.ryazanov@chaintrust.it>
2021-03-18 12:30:04 +0300

    feat(server): connect to test API

--- src/pages/Clients.js
+++ src/pages/Clients.js
@@ -1,9 +1,37 @@
-import React from "react";
-import { WithRouter } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import Loader from "@material-ui/core/CircularProgress";
 
-const Clients = (props) => {
-    console.log(props);
-    return <h1>Страница Клиенты</h1>;
+const Clients = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    setLoading(true);
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const json = await response.json();
+    setUsers(json);
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      <h1>Взаимодейтвие с сервером</h1>
+      <button onClick={fetchData}>Загрузить данные</button>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Clients;
